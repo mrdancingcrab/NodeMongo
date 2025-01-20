7a19827f8c6781935723ee23f8572df851ca860b
mrdancingcrab <jaederberg@gmail.com>
2025-01-20 17:11:47 +0100

    added express boiler  plate

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log("Yo, my name is André 3000")
+});
