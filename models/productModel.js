0c589ae6ceb067a6953d973f52856613b4d85f6d
mrdancingcrab <jaederberg@gmail.com>
2025-01-20 19:54:30 +0100

    added more endpoints

---
+++ models/productModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+    {
+       name: {
+        type: String,
+        required: true
+       },
+       quantity: {
+        type: Number,
+        required: true,
+        default: 0
+       },
+       price: {
+        type: Number,
+        required: true
+       },
+       image: {
+        type: String,
+        required: false
+       }
+    }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = 3000;
+const Product = require("./models/productModel");
+app.use(express.json());
 
 
 app.get("/", (req, res) =>{
@@ -12,15 +15,57 @@ app.post("/blog", (req,res) =>{
     res.send("This is a post req")
 });
 
+app.put("/api/product/:id", async (req,res) =>{
+
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        if(!product){
+            return res.status(400).json({message: "product does not exist"});
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json(product);
+    }
+   
+})
+
+app.get("/api/product/:id", async (req,res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500);
+    }
+});
+
+app.get("/api/products", async (req,res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
 
 
-
+app.post("/api/product", async (req,res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(404);
+    }
+});
 
 
 
 
 mongoose.
-connect("mongodb+srv://mrdancingcrab:123456admin@tobiasapi.0oqsv.mongodb.net/Products-API?retryWrites=true&w=majority&appName=TobiasAPI")
+connect(process.env.MONGO_URI)
 .then(() =>{
     console.log("Connected to mongo");
     app.listen(PORT, () => {
