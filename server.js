202ef998ab154c658fba3d552f598dddb85251ff
mrdancingcrab <jaederberg@gmail.com>
2025-01-20 18:08:22 +0100

    added connection to mongodb

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const PORT = 3000;
 
 
+app.get("/", (req, res) =>{
+    res.send("Welcome to my mongoAPI")
+});
+
+app.post("/blog", (req,res) =>{
+    res.send("This is a post req")
+});
 
 
 
-app.listen(PORT, () => {
-    console.log("Yo, my name is André 3000")
+
+
+
+
+
+mongoose.
+connect("mongodb+srv://mrdancingcrab:123456admin@tobiasapi.0oqsv.mongodb.net/Products-API?retryWrites=true&w=majority&appName=TobiasAPI")
+.then(() =>{
+    console.log("Connected to mongo");
+    app.listen(PORT, () => {
+        console.log("Yo, my name is André 3000")
+    });
+    
 });
